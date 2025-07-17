61b3094bc49dc72cee5aff7c883e373085af8e26
Salvius10 <melvinsalvius.i@gmail.com>
2025-07-18 02:17:23 +0530

    Project completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
-
+import { toast } from 'react-toastify';
+import { motion } from "framer-motion"
 const Contact = () => {
+    const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "eb7c3b8d-0306-40d0-828b-d1af95132865");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      toast.success("Form Submitted Successfully")
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      toast.error(data.message)
+      setResult("");
+    }
+  };
+
   return (
-    <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact '>
+    <motion.div 
+    initial={{opacity:0,x:-200}}
+        transition={{duration:1.5}}
+        whileInView={{opacity:1,x:0}}
+        viewport={{once:true}}
+    className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact '>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact<span className='underline underline-offset-4 decortion-1 under font-light'>With Us</span></h1>
       <p className='text-center text-gray-500 mb-8 ma-w-80 mx-auto'>Ready to Make a Move? Let's Build Your Future Together</p>
-      <form action="" className='max-w-2xl mx-auto text-gray-600 pt-8'>
+      <form onSubmit={onSubmit} className='max-w-2xl mx-auto text-gray-600 pt-8 cursor-pointer'>
         <div className='flex flex-wrap'>
             <div className='w-full md:w-1/2 text-left'>
                 Your Name
@@ -20,9 +53,9 @@ const Contact = () => {
             Message
             <textarea name="Message" placeholder='Message' required className='w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none'></textarea>
         </div>
-        <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded'>Send Message</button>
+        <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded cursor-pointer'>{result?result:'Send Message'}</button>
       </form>
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import {assets, projectsData} from '../assets/assets'
+import { motion } from "framer-motion"
 const Projects = () => {
     const [currentIndex,setCurrentIndex]=useState(0);
     const [cardsToShow,setCardsToShow]=useState(1);
@@ -23,7 +24,12 @@ const Projects = () => {
         setCurrentIndex((prevIndex)=>prevIndex===0?projectsData.length-1:prevIndex-1)
     }
   return (
-    <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
+    <motion.div
+    initial={{opacity:0,x:-200}}
+        transition={{duration:1.5}}
+        whileInView={{opacity:1,x:0}}
+        viewport={{once:true}}
+    className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decortion-1 under font-light'>Completed</span></h1>
       <p className='text-center text-gray-500 mb-8 ma-w-80 mx-auto'>Crafting Spaces, Building Legacie-Explore Our Portfolio</p>
       <div className='flex justidy-end items-center mb-8'>
@@ -54,7 +60,7 @@ const Projects = () => {
             ))}
         </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
