486f7f32794069f3a4b12216d629c0fd16bdd44f
Salvius10 <melvinsalvius.i@gmail.com>
2025-07-17 22:22:18 +0530

    Project component finished

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
-
+import {assets} from "../assets/assets"
 const About = () => {
   return (
-    <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32'>
-      
+    <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id='About'>
+      <h2 className='text-2xl sm:text-4xl font-bold mb-2'>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h2>
+      <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate about properties , Dedicated to your Vision</p>
+      <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
+        <img src={assets.brand_img} alt="" className='w-full sm:w-1/2 max-w-lg'/>
+        <div className='flex flex-col items-center md:items-start mt-10 text-gray-600'>
+            <div className='grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28'>
+                <div>
+                    <p className='text-4xl font-medium text-gray-800'>10+</p>
+                    <p>Years of Excellence</p>
+                </div>
+                <div>
+                    <p className='text-4xl font-medium text-gray-800'>12+</p>
+                    <p>Projects Completed</p>
+                </div>
+                <div>
+                    <p className='text-4xl font-medium text-gray-800'>20+</p>
+                    <p>Mn. Sq. Ft. Delivered</p>
+                </div>
+                <div>
+                    <p className='text-4xl font-medium text-gray-800'>25+</p>
+                    <p>Ongoing Project</p>
+                </div>
+            </div>
+            <p className='my-10 max-w-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, sapiente porro aut quis voluptas blanditiis et! Voluptatum consectetur nam voluptatibus numquam, rerum est obcaecati, et rem veritatis eos tenetur distinctio nulla. Accusantium vitae minima saepe nulla cumque similique quo corrupti dolor, rem odit quas ipsum nesciunt aspernatur laudantium quibusdam delectus!</p>
+            <button className='bg-blue-600 text-white px-8 py-2 rounded'>Learn More</button>
+        </div>
+      </div>
     </div>
   )
 }
