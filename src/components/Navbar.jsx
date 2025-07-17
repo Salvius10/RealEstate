3d2052ead783f151474838237b4c13995e77316d
Salvius10 <melvinsalvius.i@gmail.com>
2025-07-18 01:03:39 +0530

    Contact component finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
             <img src={assets.logo} alt="" />
             <ul className='hidden md:flex gap-7 text-white'>
-                <a href="#Home" className='cursor-pointer hover:text-gray-400'>Home</a>
+                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
                 <a href="#About" className='cursor-pointer hover:text-gray-400'>About</a>
                 <a href="#Projects" className='cursor-pointer hover:text-gray-400'>Projects</a>
                 <a href="#Testimonials" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -24,7 +24,7 @@ const Projects = () => {
     }
   return (
     <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
-      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Prokects <span className='underline underline-offset-4 decortion-1 under font-light'>Completed</span></h1>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decortion-1 under font-light'>Completed</span></h1>
       <p className='text-center text-gray-500 mb-8 ma-w-80 mx-auto'>Crafting Spaces, Building Legacie-Explore Our Portfolio</p>
       <div className='flex justidy-end items-center mb-8'>
         <button className='p-3 bg-gray-200 rounded mr-2 cursor-pointer' aria-label='Previous Project' onClick={prevProject}>
@@ -46,7 +46,7 @@ const Projects = () => {
                             {project.title}
                         </h2>
                         <p>
-                            {project.price} <span></span>{project.location}
+                            {project.price} <span className='px-1'></span>{project.location}
                         </p>
                         </div>
                     </div>
